5a061e40a871134ec5537eb2262095ec3958987c
zocc-n <znanic@yahoo.com>
2022-12-15 22:07:23 +0100

    Initial functions to be changed later

---
+++ script.js
@@ -0,0 +1,43 @@
+let result;
+
+
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(num1, num2){
+    return num1-num2;
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+function divide(num1, num2){
+    return num1/num2;
+
+}
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1. num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}
+
+let numButtons = document.querySelector('#numButtons');
+
+numButtons.addEventListener('click', test);
+
+function test();
